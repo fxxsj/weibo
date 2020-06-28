2404e47dbf54972f0b64941c655cdbb9f767236f
MBA <garhing@sina.com>
2020-06-28 14:39:13 +0000

    登录成功后导航逻辑

--- public/js/app.js
+++ public/js/app.js
@@ -37292,8 +37292,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/chenjiaxing/Code/weibo/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/chenjiaxing/Code/weibo/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/Code/weibo/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vagrant/Code/weibo/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
